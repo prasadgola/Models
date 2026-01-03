0c74ee9e20ea6efbe778517daba807254c8a267c
prasadgola <tobasavaprasad@gmail.com>
2026-01-02 20:10:57 -0600

    convert text data to token ids

--- gpt2/gpt2.py
+++ gpt2/gpt2.py
@@ -2,4 +2,14 @@ import os
 import tiktoken
 
 
-print(tiktoken.encoding_for_model("gpt-2").encode("Hello Universe"))
+tokenizer = tiktoken.encoding_for_model("gpt-2")
+
+with open("training_data.txt", "r", encoding="utf-8") as f:
+    training_data = f.read()
+
+# convert traning data to token ids - (encoder)
+training_token_ids = tokenizer.encode(training_data, allowed_special={"<|space|>"})
+
+
+
+
