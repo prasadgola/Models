d8ffc64448f2b714e9802a295bcd724d0bf5cd27
prasadgola <tobasavaprasad@gmail.com>
2026-01-11 01:02:00 -0600

    test traning

--- gpt2/gpt2.py
+++ gpt2/gpt2.py
@@ -1,5 +1,6 @@
 import tiktoken
 import torch
+from torch.utils.data import Dataset, DataLoader
 
 class model(torch.nn.Module):
     def __init__(self, variables):
@@ -139,32 +140,38 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
     return idx
 
 
+class Dataset(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # Tokenize the entire text
+        token_ids = tokenizer.encode(txt, allowed_special={"<|endoftext|>"})
+
+        # Use a sliding window to chunk the book into overlapping sequences of max_length
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
 
 
+def create_dataloader(txt, batch_size=2, max_length=256,
+                         stride=128, shuffle=True, drop_last=True, num_workers=0):
+    # Initialize the tokenizer
+    tokenizer = tiktoken.get_encoding("gpt2")
 
+    # Create dataset
+    dataset = Dataset(txt, tokenizer, max_length, stride)
 
+    # Create dataloader
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
 
-torch.manual_seed(123)
-
-# input to the untrained model
-input_text = "Hello Universe, glad to meet yo"
-
-encoded = tiktoken.get_encoding("gpt2").encode(input_text)
-encoded_tensor = torch.tensor(encoded).unsqueeze(0)
-
-
-
-
-
-#  and hardware compatibility
-intial_state = {"tokens": 50257, "token_dimension": 768, "context_length": 1024, "drop_rate": 0.1, "heads": 12, "layers": 12, "qkv_bias": False}
-model = model(intial_state)
-
-out = generate_text_simple(
-    model=model,
-    idx=encoded_tensor, 
-    max_new_tokens=6, 
-    context_size=intial_state["context_length"]
-)
-
-print(tiktoken.get_encoding("gpt2").decode((out.squeeze(0)).tolist()))
+    return dataloader
